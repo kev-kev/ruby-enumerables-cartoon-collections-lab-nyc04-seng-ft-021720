0e90445b41aabc98868b3ce7853160487344a530
Kevin Clark <kevin.clark93@gmail.com>
2020-02-10 18:13:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def roll_call_dwarves(names_arr)
   names_w_nums
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(call_array)
+  
 end
 
 def long_planeteer_calls# code an argument here
