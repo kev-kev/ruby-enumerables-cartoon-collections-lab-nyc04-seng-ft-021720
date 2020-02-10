2b1da45ef6c269c3f78613abe2f65aea4e2387dd
Kevin Clark <kevin.clark93@gmail.com>
2020-02-10 18:27:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ def find_the_cheese(cheese_arr)
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_arr.each{ |item|
     if cheese_types.include?(item)
-      break return item
+      return item
     end
     return nil
   }
